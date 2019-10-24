36b15a6ffb52348db414a036d6daf4add7238382
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-10-24 10:24:55 -0400

    prenumber generator

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,13 +1,19 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        gameLoop;
+        Scanner keyboard = new Scanner(System.in);
+
+        int pickedNumber;
+
+        for (int i = 0; i <1; i++){
+            pickedNumber = (int) (Math.random()*100)+1;
+            System.out.println(pickedNumber);
+        }
     }
 
 
-    public static void gameLoop {
-
-    }
 }
