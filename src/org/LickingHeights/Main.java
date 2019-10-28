085cc2915602fcf1c09fb85bb651167a4a7faa3c
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-10-28 09:48:43 -0400

    Beginning of number guessing game
    Welcome message, and how to play

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -5,14 +5,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String response;
         Scanner keyboard = new Scanner(System.in);
-
         int pickedNumber;
 
-        for (int i = 0; i <1; i++){
-            pickedNumber = (int) (Math.random()*100)+1;
-            System.out.println(pickedNumber);
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("For this game, you must try to guess the number I am thinking about between the numbers 1 and 100");
+        System.out.println("Let's start off by you taking your first guess.");
+
+        //logic
+        int randomNumber = (int) (Math.random()*100)+1;
+
+        do {
+            pickedNumber = keyboard.nextInt();
+            System.out.println(logic(pickedNumber, randomNumber));
+        } while (pickedNumber != randomNumber);
+
+        
+
+
+
+            //game loop
+
+            System.out.println("Do you want to play again?");
+            response =keyboard.next();
+
+            //logic method
+
         }
+
     }
 
 
